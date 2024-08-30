f0eee69b679ae28257212d799a28d38ec676d046
Corey Schomer <coreyschomer@gmail.com>
2024-08-30 15:25:55 -0600

    chore: Update vite.config.ts to import from "vitest/config" instead of "vite"

--- 13-caching/react-app/vite.config.ts
+++ 13-caching/react-app/vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from "vitest/config";
+// <reference types="vitest" />
+import { defineConfig } from "vite";
+import "vitest/config"; // this line actually pulls in types
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
